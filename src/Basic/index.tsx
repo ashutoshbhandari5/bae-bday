10350113a0537084c1daf565c683b0585639407d
Ashutosh Bhandari <ashutoshbhandari5@gmail.com>
2025-02-01 21:03:13 -0500

    fixed vercel issue

--- src/Basic/index.tsx
+++ src/Basic/index.tsx
@@ -232,11 +232,12 @@ export default function BasicDemo(props: Props) {
             <img src="./future.jpeg" alt="firstTime" height={120} width={350} />
           </div>
         </Section>
-        <div className="flex justify-center items-center">
+        <div style={{ padding: '20px' }} className="flex justify-center items-center">
           <button
             onClick={() => {
               window.open('https://bday-celebration.vercel.app/', '_blank');
             }}
+            style={{ height: '100px', cursor: 'ponter', fontSize: '18px' }}
             className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full"
           >
             Let's celebrate🎉
